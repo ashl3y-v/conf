9e83c97aaa86db370c8468c76633ae2fd7b4f707
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-03-31 21:54:18 -0700

    switch back to wezterm & btop

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 7.25
+
+config.default_prog = { "bash" }
+
+config.color_scheme = "GruvboxDark"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.warn_about_missing_glyphs = false
+config.window_frame = {
+  font_size = 7.25
+}
+config.colors = {
+  tab_bar = {
+    background = "#282828",
+    active_tab = {
+      bg_color = "#282828", fg_color = "#b8bb26", intensity="Bold"
+    },
+    inactive_tab = {
+      bg_color = "#282828", fg_color = "#ebdbb2"
+    },
+    new_tab = {
+      bg_color = "#282828", fg_color = "#b8bb26", intensity="Bold"
+    },
+  },
+}
+
+return config
