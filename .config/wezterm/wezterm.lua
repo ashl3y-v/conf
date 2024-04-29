984901f73fe7b3e962afc207b3eb06d3a4bce90b
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-04-28 21:16:11 -0700

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,11 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font {
+  family = "JetBrains Mono",
+  harfbuzz_features = { "cv06=1", "cv14=1", "cv15=1" }
+}
+
 config.font_size = 7.25
 
 config.default_prog = { "bash" }
@@ -13,20 +17,17 @@ config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.warn_about_missing_glyphs = false
-config.window_frame = {
-  font_size = 7.25
-}
 config.colors = {
   tab_bar = {
     background = "#282828",
     active_tab = {
-      bg_color = "#282828", fg_color = "#b8bb26", intensity="Bold"
+      bg_color = "#282828", fg_color = "#b8bb26", intensity = "Bold"
     },
     inactive_tab = {
       bg_color = "#282828", fg_color = "#ebdbb2"
     },
     new_tab = {
-      bg_color = "#282828", fg_color = "#b8bb26", intensity="Bold"
+      bg_color = "#282828", fg_color = "#b8bb26", intensity = "Bold"
     },
   },
 }
