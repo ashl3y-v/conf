eb7f8b8f5964aae58de9c3fded8f3251bd2c33dd
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-05-29 13:15:00 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 sudo pacman -Syyu --needed --noconfirm git base-devel rustup
 
 rustup default stable
 rustup update
 
 mkdir -p ~/g/ ~/n/ ~/.ssh/ ~/.ssh/sockets/
-cd ~/t
+cd ~/n
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -i
 
-sudo rm -rf ~/t/paru
+sudo rm -rf ~/n/paru
 
 paru -S --needed --noconfirm firefox helix jq
 
-sudo vim /etc/ssh/sshd_config
+sudo helix /etc/ssh/sshd_config
 sudo systemctl daemon-reload
+
+sudo systemctl enable sshd
+sudo systemctl enable systemd-oomd
+
+git config --global fetch.prune true
+git config --global pull.rebase true
+
+# xdg-mime default org.pwmt.zathura.desktop application/pdf
--- setup/misc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo systemctl enable systemd-oomd.service
-sudo systemctl enable tor
-sudo systemctl enable sshd
-
-git config --global fetch.prune true
-git config --global pull.rebase true
-
-xdg-mime default org.pwmt.zathura.desktop application/pdf
